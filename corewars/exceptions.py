b8d8b438b7f65accbd5fc51871acffea13c0c5ee
Piotrek Brzeziński <thewildtree@outlook.com>
2021-01-15 19:24:37 +0100

    Move source files to a separate folder

--- corewars/exceptions.py
+++ corewars/exceptions.py
@@ -1,3 +1,4 @@
 class ParserException(Exception):
     def __init__(self, line_number: int, line_content: str, message: str):
-        super().__init__(f'{message} -- line {line_number}, content: {line_content}')   
+        super().__init__(f'{message} -- line {line_number}, content: {line_content}')
+
