7b56ef21688fff729a7a8be13cc839af4e7a9ca9
Piotrek Brzeziński <thewildtree@outlook.com>
2021-01-18 12:39:50 +0100

    Implement cycling through warriors in the Core

--- corewars/core.py
+++ corewars/core.py
@@ -11,7 +11,8 @@ class Core():
     def __init__(self, size=8000):
         self._size = size
         self._instructions: List[CoreInstruction]
-        self.warriors: List[CoreWarrior]
+        self._warriors: List[CoreWarrior]
+        self._warrior_index: int
         self.clear()
 
 
@@ -19,7 +20,8 @@ class Core():
         self._instructions = []
         for _ in range(self._size):
             self._instructions.append(CoreInstruction(self, default_instruction))
-        self.warriors = []
+        self._warriors = []
+        self._warrior_index = 0
 
 
     def load_warrior(self, warrior: Warrior, address: int):
@@ -32,11 +34,26 @@ class Core():
             address = 2137
         # create initial process for the given warrior
         core_warrior = CoreWarrior(self, warrior.name, address)
-        self.warriors.append(core_warrior)
+        self._warriors.append(core_warrior)
         # load warrior's instructions into core
         for i, instruction in enumerate(warrior.instructions):
             core_instruction = CoreInstruction(self, instruction)
             self[address + i] = core_instruction
+
+
+    def switch_warrior(self):
+        """
+        Removes the currently active warrior if it does not have any active processes anymore.
+        Changes the 'active' warrior to the next one on the list.
+        """
+        if len(self.current_warrior) == 0:
+            self._remove_current_warrior()
+        self._warrior_index = (self._warrior_index + 1) % len(self._warriors)
+
+
+    @property
+    def current_warrior(self):
+        return self._warriors[self._warrior_index]
 
 
     def normalize_value(self, value: int) -> int:
@@ -46,6 +63,20 @@ class Core():
         while value < 0:
             value += self._size
         return value
+
+
+    def _remove_current_warrior(self):
+        """
+        Rremoves the current warrior from the core.
+        Requires next_warrior() to be called afterwards to ensure proper behaviour.
+        """
+        self._warriors.remove(self._warriors[self._warrior_index])
+        # in most cases switch backwards (turn_next() will correctly jump to next process afterwards)
+        if self._warrior_index != 0:
+            self._warrior_index -= 1
+        # removing first process - switch to the last one so that turn_next() will circle back to the beginning
+        else:
+            self._warrior_index = len(self._warriors) - 1
 
 
     def __getitem__(self, key):
@@ -117,15 +148,19 @@ class CoreWarrior():
     def __init__(self, core: Core, name: str, initial_address: int):
         self.name = name
         self._core = core
-        self._current_process = 0
+        self._current_index = 0
         self._processes: List[int] = []
         # a list of integers - each one is an instruction pointer for one process
         # pointers contain absolute Core memory addresses.
         self.add_process(initial_address)
 
 
-    def turn_next(self):
-        self._current_process = (self._current_process + 1) % len(self._processes)
+    def __len__(self):
+        return len(self._processes)
+
+
+    def next_process(self):
+        self._current_index = (self._current_index + 1) % len(self._processes)
 
 
     def add_process(self, starting_address: int):
@@ -141,22 +176,22 @@ class CoreWarrior():
         Simply removes the current proccess from the list.
         Requires turn_next() to be called afterwards to ensure proper behaviour.
         """
-        self._processes.remove(self._processes[self._current_process])
+        self._processes.remove(self._processes[self._current_index])
         # in most cases switch backwards (turn_next() will correctly jump to next process afterwards)
-        if self._current_process != 0:
-            self._current_process -= 1
+        if self._current_index != 0:
+            self._current_index -= 1
         # removing first process - switch to the last one so that turn_next() will circle back to the beginning
         else:
-            self._current_process = len(self._processes) - 1
+            self._current_index = len(self._processes) - 1
 
 
     @property
     def current_pointer(self) -> int:
         "Returns an instruction pointer of the process currently being executed."
-        return self._processes[self._current_process]
+        return self._processes[self._current_index]
 
 
     @current_pointer.setter
     def current_pointer(self, value: int):
         # in case we're at coreSize-1 and increment, for example
-        self._processes[self._current_process] = self._core.normalize_value(value)
+        self._processes[self._current_index] = self._core.normalize_value(value)
